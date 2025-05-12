5a8b454cb1d5c1e1660f56750cc059fcfbcc6344
Nelio Alves <acenelio@gmail.com>
2025-05-12 15:30:25 -0300

    Project created

---
+++ src/test/java/com/devsuperior/aulasql/AulasqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.aulasql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AulasqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
